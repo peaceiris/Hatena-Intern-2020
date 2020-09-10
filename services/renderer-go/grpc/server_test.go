7837b5b8bc6f57bbc031c8922bcb6e43a2bc72a2
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-09-10 17:38:01 +0900

    test: Add Test_Server_Render_OGPImage

--- services/renderer-go/grpc/server_test.go
+++ services/renderer-go/grpc/server_test.go
@@ -44,3 +44,21 @@ func Test_Server_Render(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "<p>foo <a href=\"https://google.com/\">https://google.com/</a> bar</p>\n", reply.Html)
 }
+
+func Test_Server_Render_OGPImage(t *testing.T) {
+	fecherCli := &fakeFecherClient{
+		FakeFetch: func(ctx context.Context, req *pb_fetcher.FetchRequest, opt ...grpc.CallOption) (*pb_fetcher.FetchReply, error) {
+			return &pb_fetcher.FetchReply{Title: "Example Domain"}, nil
+		},
+	}
+	ogpImageFecherCli := &fakeOGPImageFecherClient{
+		FakeFetch: func(ctx context.Context, req *pb_image_fetcher.FetchRequest, opt ...grpc.CallOption) (*pb_image_fetcher.FetchReply, error) {
+			return &pb_image_fetcher.FetchReply{Url: "https://example.com/images/ogp.jpg"}, nil
+		},
+	}
+	s := NewServer(fecherCli, ogpImageFecherCli)
+	src := "{{ preview \"https://google.com/\" }}"
+	reply, err := s.Render(context.Background(), &pb.RenderRequest{Src: src})
+	assert.NoError(t, err)
+	assert.Equal(t, "<p><a href=\"https://google.com/\">Example Domain</a>\n<img src=\"https://example.com/images/ogp.jpg\" alt=\"\" /></p>\n", reply.Html)
+}
